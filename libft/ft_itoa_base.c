e1e3ccb78b63aed666525c2bebaaaf183953d985
Oskari Rantanen <orantane@c2r2p6.hive.fi>
2020-02-04 17:51:06 +0200

    Created and added itoa base function to the library, so that I can print hexadecimals

--- libft/ft_itoa_base.c
+++ libft/ft_itoa_base.c
@@ -6,60 +6,64 @@
 /*   By: orantane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/04 14:56:56 by orantane          #+#    #+#             */
-/*   Updated: 2020/02/04 15:54:53 by orantane         ###   ########.fr       */
+/*   Updated: 2020/02/04 17:44:51 by orantane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char		*ft_rev(char *str)
+static char		*ft_rev(char *str, int neg)
 {
 	size_t	i;
 	char	*rev;
 	size_t	len;
 
-	i = 0;
+	i = -1;
 	len = ft_strlen(str);
-	rev = ft_strnew(len);
-	while (len > 0)
+	if (neg != 0)
 	{
-		rev[i] = str[len];
-		i++;
-		len--;
+		rev = ft_strnew(len + 1);
+		rev[++i] = '-';
 	}
+	else
+		rev = ft_strnew(len);
+	while (len > 0)
+		rev[++i] = str[--len];
 	return (rev);
 }
 
-static char		*ft_hexa(int num, char *str)
+static char		*ft_hexa(int num, char *str, int neg)
 {
 	int		i;
 	int		temp;
 
-	i = 0;
+	i = -1;
+	if (num < 0)
+	{
+		num = num * -1;
+		neg = 1;
+	}
 	while (num != 0)
 	{
+		temp = 0;
 		temp = num % 16;
 		if (temp < 10)
-		{
-			str[i] = temp + 48;
-			i++;
-		}
+			str[++i] = temp + '0';
 		else
-		{
-			str[i] = temp + 55;
-			i++;
-		}
+			str[++i] = temp - 10 + 'a';
 		num = num / 16;
 	}
-	return (str);
+	return (ft_rev(str, neg));
 }
 
-char		*ft_itoa_base(int num, int base)
+char			*ft_itoa_base(int num, int base)
 {
 	int		i;
 	char	*str;
 	size_t	len;
+	int		neg;
 
+	neg = 0;
 	len = 100;
 	i = 0;
 	if (base == 0)
@@ -68,6 +72,6 @@ char		*ft_itoa_base(int num, int base)
 		return (ft_itoa(num));
 	str = ft_strnew(len);
 	if (base == 16)
-		str = ft_hexa(num, str);
+		str = ft_hexa(num, str, neg);
 	return (str);
 }
